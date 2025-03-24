b9f180286a2bda85e5e703d2571f2f8d4b41e7c3
Keegan Veazey <113397615+keeganveazey@users.noreply.github.com>
2025-03-24 19:45:33 -0400

    Updated code quality (pt.1)
    - need to add error handling + test cases

--- data_processing_pipeline.py
+++ data_processing_pipeline.py
@@ -1,3 +1,18 @@
+# FILE DESCRIPTION: -------------------------------------------------------
+
+# This file run the data preprocessing pipeline. The purpose of this pipeline is to automate the standardization
+# and preprocessing techniques applied to all raw data images, split the data using a custom dataset split
+# into training, validation, and test which maintains class folders for readability and user organization.
+# It also applies training data augmentation . The raw data is in the DATA folder (available on github),
+# and can be run directly through this pipeline.
+
+# The preprocessing techniques include scaling, padding and normalizing, and duplicate removal
+# images and splits the data . For data augmentation: changes include rotations, brightness enhancement, and mirroring
+
+# --------------------------------------------------------------------------
+
+# IMPORTS
+
 from undouble import Undouble
 import os
 import glob
@@ -9,17 +24,51 @@ from collections import defaultdict
 import random
 import shutil
 
-# Directory constants
-RAW_IMG_DIR = "DATA/"
+# For reproducibility and consistent data split
+random.seed(2025)
 
 # Image extensions
 EXTENSIONS = ["*.jpg", "*.jpeg", "*.png"]
 
+# Directory constants
+RAW_IMG_DIR = "DATA/"
+ALL_PROCESSED_DATA = "PROCESSED_DATA/"
+VALIDATION_DATA = f'{ALL_PROCESSED_DATA}VALIDATION_DATA/'
+TEST_DATA = f'{ALL_PROCESSED_DATA}TEST_DATA/'
+TRAINING_DATA = f'{ALL_PROCESSED_DATA}TRAINING_DATA/'
+AUGMENTED_OUTPUT_DIR = f'{TRAINING_DATA}TRAINING_AUGMENTED_DATA/'
+
+DIR_LIST = [ALL_PROCESSED_DATA, VALIDATION_DATA, TEST_DATA, TRAINING_DATA, AUGMENTED_OUTPUT_DIR]
+
+# Augmentation constants
+AUGMENTATION_LIST = ["original", "flipped_horizontally", "flipped_vertically", \
+                     "rotated_90", "rotated_180", "brightness_enhanced"]
+
+
+# Map of filename patterns to the correct class name
+INSECT_CLASS_MAPPING = {
+    "weevil": "adult_rice_weevil",
+    "centipede": "house_centipede",
+    "house_spider": "american_house_spider",
+    "house_": "american_house_spider",
+    "bedbug": "bedbug",
+    "stink": "brown_stink_bug",
+    "carpenterant": "carpenter_ant",
+    "cellar": "cellar_spider",
+    "flea": "flea",
+    "silverfish": "silverfish",
+    "termite": "subterranean_termite",
+    "tick": "tick",
+}
 
 def rename_files(directory):
     """
     Renames files in the directory by replacing spaces and extra dots with underscores
+
+    Parameters:
+        directory (str) - directory where raw data is stored
     """
+
     for ext in EXTENSIONS:
         for file_path in glob.glob(os.path.join(directory, "**", ext), recursive=True):
             dir_name = os.path.dirname(file_path)
@@ -37,9 +86,13 @@ def rename_files(directory):
 
 def process_duplicates(directory, threshold=0):
     """
-    Finds and removes duplicate images
-    # Undouble reference: https://erdogant.github.io/undouble/pages/html/input_output.html
+    Finds and removes duplicate images using Undouble - reference: https://erdogant.github.io/undouble/pages/html/input_output.html
+
+    Parameters:
+        directory (str) - directory where raw data is stored,
+        threshold (int) - default 0, threshold used to detect identical hash values
     """
+
     # Init undouble with default settings
     model = Undouble()
 
@@ -70,62 +123,48 @@ def process_duplicates(directory, threshold=0):
 
         if len(idx_group) == 1:
             non_duplicate_images.append(group_images[0])
+
         else:
             keep_image = group_images[0]
             remove_images = group_images[1:]
 
-            print(f"Keeping: {keep_image}")  # Print which image is kept
+            # Show which image was just kept
+            print(f"Keeping: {keep_image}")
 
             for img_path in remove_images:
                 try:
                     os.remove(img_path)
                     duplicate_images.append(img_path)
                     print(f"Removed duplicate: {img_path}")
+
                 except FileNotFoundError:
                     print(f"File not found (could have already been moved): {img_path}")
 
     return duplicate_images, non_duplicate_images
 
 
-# for reproducibility and consistent data split
-random.seed(2025)
-
-# Directory constants
-RAW_IMG_DIR = "DATA/"
-ALL_PROCESSED_DATA = "PROCESSED_DATA/"
-
-VALIDATION_DATA = f'{ALL_PROCESSED_DATA}VALIDATION_DATA/'
-TEST_DATA = f'{ALL_PROCESSED_DATA}TEST_DATA/'
-
-TRAINING_DATA = f'{ALL_PROCESSED_DATA}TRAINING_DATA/'
-AUGMENTED_OUTPUT_DIR = f'{TRAINING_DATA}TRAINING_AUGMENTED_DATA/'
-
-# List constants
-DIR_LIST = [ALL_PROCESSED_DATA, VALIDATION_DATA, TEST_DATA, \
-            TRAINING_DATA, AUGMENTED_OUTPUT_DIR]
-
-AUGMENTATION_LIST = ["original", "flipped_horizontally", "flipped_vertically", \
-                     "rotated_90", "rotated_180", "brightness_enhanced"]
-
-
-# ------- FUNCTIONS -------
-
 def make_directories(dir_lst):
-    '''
-    Params: dir_lst - list of directories
-    Function: makes
-    '''
+    """
+    Makes directories in the provided list
+
+    Parameters:
+        dir_lst (list) - list of directories to make
+    """
+
     for d in dir_lst:
         os.makedirs(d, exist_ok=True)
     print('Made necessary directories.')
 
 
 def get_folders(directory):
-    '''
-    Param: directory - str
-    Function: gets all subfolders in directory
+    """
+    Gets list of all subfolders in directory
+
+    Parameters:
+        directory (list) - list of directory strings to get folders from
+
     Returns: list of folder names
-    '''
+    """
     folders = []
     for entry in os.scandir(directory):
         if entry.is_dir():
@@ -134,13 +173,14 @@ def get_folders(directory):
 
 
 def move_random_files(insect_source_dir, dest_dir, num_files):
-    '''
-    Param: source_dir - str, dest_dir - str, num_files - int
-    Function: move specified number of files to desired folder
-                choosing randomly (w/o replpacement)
-    Returns: specified number of random files from source
-                to destination directory
-    '''
+    """
+    Moves specified number of files to desired folder choosing randomly (without replacement)
+
+    Parameters:
+        insect_source_dir (str) - directory of insect
+        dest_dir (str) - destination set directory
+        num_files (int) - number of files to move from source to destination
+    """
 
     # getting all files in this insect class
     files_in_source = []
@@ -166,11 +206,14 @@ def move_random_files(insect_source_dir, dest_dir, num_files):
 
 
 def get_image_file(img_path):
-    '''
-    Param: img_path - str
-    Function: Splits the string from last occurrence of forward slash.
-    Returns: filename as str
-    '''
+    """
+    Splits the string from last occurrence of forward slash
+
+    Parameters:
+        img_path (str) - path of image
+
+    Returns: filename as string
+    """
 
     filename = img_path.rsplit('/', 1)[-1]
     return filename
@@ -178,43 +221,56 @@ def get_image_file(img_path):
 
 # reference: https://stackoverflow.com/questions/44231209
 def make_square_by_padding(image, min_size=224, fill_color='w'):
-    '''
-    Params: image - Image object, min_size - int, fill_color - 'w' or 'b'
-    Function: pad rectangular images centrally to avoid stretching
-            and comprimising integrity of image
-    '''
+    """
+    Pads rectangular images centrally to avoid stretching and compromising integrity of image
 
+    Parameters:
+        image (Image object) - image to make square
+        min_size (int) - edge size to scale to
+        fill_color (str) - 'w' for white background or 'b' for black background
+
+    Returns:
+        Image object
+    """
+
+    # set background color
     if fill_color == 'b':
         fill_color_code = (0, 0, 0, 0)
 
     elif fill_color == 'w':
         fill_color_code = (255, 255, 255, 0)
+
     else:
         raise ValueError('Incorrect fill color option: put w or b for a white or black background.')
 
     x, y = image.size
 
-    # get largest dim so not stretching
+    # get largest dim to avoid stretching
     size = max(min_size, x, y)
 
+    # fill edges as needed with desired background
     new_image = Image.new('RGBA', (size, size), fill_color_code)
 
-    # centers image
+    # centers image and pads centrally
     new_image.paste(image, (int((size - x) / 2), int((size - y) / 2)))
 
     return new_image
 
 
 def resize_and_recolor(image_path, desired_square_dim=224, verbose=False):
-    '''
-    Param: image_path - str, desired_square_dim - int, verbose - boolean
-    Function:
-        # pad centrally to keep aspect ratio of rectangular images
-        # convert to RBG format
-        # standardize to desired size
+    """
+    Pads centrally to keep aspect ratio of rectangular images, converts image
+    to RBG format, standardizes image to desired size.
 
-    Returns: standardized image object (none if NA), log as dict of image errors if applicable
-    '''
+    Parameters:
+        image_path (str) - path of image file
+        desired_square_dim (int) - number of pixels for desired edge size
+        verbose (boolean) - show print statements or not
+
+    Returns:
+        (Image Object) standardized image object (none if NA)
+        (dict) log as dict of image errors if applicable
+    """
 
     error_log = defaultdict(list)
     img_name = get_image_file(image_path)
@@ -246,16 +302,20 @@ def resize_and_recolor(image_path, desired_square_dim=224, verbose=False):
 
 
 def augment_image(img_object, output_subdir, base_filename, verbose=False):
-    '''
-    Param: img_object - Image object, output_subdir - str,
-            base_filename - str, verbose - boolean
-    Function:
-        - create new images by flipping each horizontally,
-        vertically, rotate, and brightness
-        - saves augmented images in output folder
+    """
+    Creates new images by flipping each horizontally, vertically, rotate,
+    and adjusts brightness. Saves augmented images in output training folder
 
-    Returns: boolean reflection image augmentation success, log as dict of image errors if applicable
-    '''
+    Parameters:
+        img_object (Image object) - image to augment
+        output_subdir (str) - directory to save augmented image to
+        base_filename (str) - name of image file without extension
+        verbose (boolean) - show print statements or not
+
+    Returns: log as dict of image errors if applicable
+        (boolean) flagging image augmentation success,
+        (dict) log as dict of image errors if applicable
+    """
 
     error_log = defaultdict(list)
 
@@ -293,15 +353,14 @@ def augment_image(img_object, output_subdir, base_filename, verbose=False):
 
 
 def preprocess_data(class_folders_list, raw_data_directory, desired_output_directory):
-    '''
-    Params:
-    Function: iterates through raw image files and does the following:
-        - resizes
-        - recolors
-        - creates 5 image variations (augmentation)
-    Returns: void since procedure
+    """
+    Iterates through raw image files resizes, recolors, and applies 5 image variations for augmentation
 
-    '''
+    Parameters:
+        class_folders_list (list) - list of class folders
+        raw_data_directory (str) - directory with raw data to process
+        desired_output_directory (str) - directory to save data to
+    """
 
     # counters
     total_files = 0
@@ -319,6 +378,7 @@ def preprocess_data(class_folders_list, raw_data_directory, desired_output_direc
         # make insect subfolders if DNE
         os.makedirs(output_folder, exist_ok=True)
 
+        # walk through the directory
         for root, subdirs, files in os.walk(folder_path):
             for file in files:  # for each image in RAW_IMG_DATA
                 total_files += 1
@@ -332,7 +392,7 @@ def preprocess_data(class_folders_list, raw_data_directory, desired_output_direc
                     # saving relative path to this img
                     relative_path = os.path.relpath(root, folder_path)
 
-                    # constuct path to save processed img to corresponding output folder
+                    # construct path to save processed img to corresponding output folder
                     output_subdir = os.path.join(output_folder, relative_path)
 
                     # saving info needed for output file saving
@@ -343,27 +403,33 @@ def preprocess_data(class_folders_list, raw_data_directory, desired_output_direc
 
                     # case: successfully processed img
                     if processed_img is not None:
-                        processed_img_save_path = os.path.join(output_subdir, \
-                                                               f"{base_filename}_processed.jpg")
+                        processed_img_save_path = os.path.join(output_subdir, f"{base_filename}_processed.jpg")
                         processed_img.save(processed_img_save_path)
                         successfully_processed += 1
 
                 else:
                     print(f"Skipped non-image file: {file}")
 
-    # summary
+    # show summary
     print(f"Successfully processed: {successfully_processed}, Total raw images: {total_files}")
+
     return
 
 
 def normalize_image(img_object, range_type=1):
-    '''
-    Param: image - PIL Image object,
-            range_type - int flag depicting type 1 range
-            defined as [-1,1] or type 2 range defined as [0,1]
-    Function: Normalized image as 3d NumPy array
-    Returns: NumPy array
-    '''
+    """
+    Normalizes image as 3d NumPy array
+
+    Parameters:
+        image (Image object) - image to normalize
+        range_type (int) - flag depicting type 1 range defined as [-1,1]
+                            or type 2 range defined as [0,1]
+
+    Returns:
+        NumPy array
+    """
+
+    # convert image to numpy array
     image_array = np.array(img_object).astype(np.float32)
 
     if range_type == 1:
@@ -380,13 +446,14 @@ def normalize_image(img_object, range_type=1):
 
 
 def split_data_into_train_valid_test(processed_data_folder_lst):
-    '''
-    Param: processed_data_folder_lst - list
-    Function: Splits and moves all processed data (resizd and rgb coded images)
-            into validation, test, and training sets
-    Return: void since procedure
-    '''
+    """
+    Splits and moves all processed data (resized and rgb coded images) into validation, test, and training sets
 
+    Parameters:
+        processed_data_folder_lst (list) - list of folders in processed data
+    """
+
+    # for each of the class folders, get 40-20-rest for validation-test-training split
     for folder in processed_data_folder_lst:
         source_folder_path = os.path.join(ALL_PROCESSED_DATA, folder)
 
@@ -399,18 +466,21 @@ def split_data_into_train_valid_test(processed_data_folder_lst):
         # move the rest to training
         shutil.move(source_folder_path, TRAINING_DATA)
 
+    # lmk when complete
     print('Completed splitting data into training, validation, and test sets.')
+
     return
 
 
 def perform_data_augmentation(training_folders_lst):
-    '''
-    Param: training_folders - list
-    Function: reads throough training directory and augments each image,
-              saving it to a new class specific augmented folder
-    Return: void since procedure
-    '''
+    """
+    Reads through training directory and augments each image, saving it to a new class specific augmented folder
 
+    Parameters:
+        training_folders_lst (list) - list of folders in training set
+    """
+
+    # keep count of augmented images
     successfully_augmented = 0
 
     for folder in training_folders_lst:
@@ -432,9 +502,7 @@ def perform_data_augmentation(training_folders_lst):
                 processed_img = Image.open(file_path)
 
                 # perform and save augmentations
-                final_img_success, augment_error_log = augment_image(processed_img, \
-                                                                     output_subdir, \
-                                                                     base_filename)
+                final_img_success, augment_error_log = augment_image(processed_img,output_subdir,base_filename)
 
                 if final_img_success == True:
                     successfully_augmented += 6
@@ -443,29 +511,18 @@ def perform_data_augmentation(training_folders_lst):
                     print(augment_error_log)
 
     print(f'Completed data augmentation on training set: training data now stands at {successfully_augmented} images')
+
     return
 
 
-# Map of filename patterns to the correct class name
-INSECT_CLASS_MAPPING = {
-    "weevil": "adult_rice_weevil",
-    "centipede": "house_centipede",
-    "house_spider": "american_house_spider",
-    "house_": "american_house_spider",
-    "bedbug": "bedbug",
-    "stink": "brown_stink_bug",
-    "carpenterant": "carpenter_ant",
-    "cellar": "cellar_spider",
-    "flea": "flea",
-    "silverfish": "silverfish",
-    "termite": "subterranean_termite",
-    "tick": "tick",
-}
 
 
 def organize_data(data_dir):
     """
     Create insect class folders and organize images to their correct folders
+
+    Parameters:
+        data_dir (str) - directory of data to organize by class
     """
     if not os.path.exists(data_dir):
         print(f"Directory {data_dir} does not exist!")
@@ -494,6 +551,12 @@ def organize_data(data_dir):
 
 def main():
 
+    # When running this pipeline, expect the process to take approximately 2.5 minutes - 3 minutes to complete.
+
+
+    global TEST_DATA
+    global VALIDATION_DATA
+
     start = time.time()
 
     print("---------------START OF IMAGE PROCESSING---------------")
@@ -506,7 +569,6 @@ def main():
     print("\nProcessing duplicate images---------------")
     duplicate_images, non_duplicate_images = process_duplicates(RAW_IMG_DIR)
 
-    input('pause 1')
 
     print("\nHashing and removing duplicate images process complete.")
     print(f"Total duplicates removed: {len(duplicate_images)}")
@@ -516,7 +578,6 @@ def main():
     print("---------------START OF MAKING DIRECTORIES AS NEEDED---------------")
     make_directories(DIR_LIST)
 
-    input('pause 2')
 
     # 2) get insect folders list
     print("---------------GETTING INSECT FOLDERS---------------")
@@ -547,12 +608,10 @@ def main():
 
     perform_data_augmentation(training_folders)
 
-    print("---------------SORTING FILES TO FINAL DESETINATIONS---------------")
+    print("---------------SORTING FILES TO FINAL DESTINATIONS---------------")
     PROCESSED_DATA = "PROCESSED_DATA"
     TEST_DATA = os.path.join(PROCESSED_DATA, "TEST_DATA")
     VALIDATION_DATA = os.path.join(PROCESSED_DATA, "VALIDATION_DATA")
-
-    input('pause 3')
 
     # Organize the data in the TEST_DATA and VALIDATION_DATA folders
     organize_data(TEST_DATA)
@@ -562,6 +621,18 @@ def main():
 
     end = time.time()
     print(f'Processing pipeline complete - took approximately {round(end-start)} seconds')
+
+    # --------------------------------------------------------------------------
+    # TEST CASE / EXPECTED RESULTS when this script is run:
+
+        # 85 duplicate images removed
+        # 5838 images in augmented TRAINING_AUGMENTED_DATA folder
+        # 40 images per class folder in VALIDATION_DATA folder
+        # 20 images per class in TEST_DATA folder
+
+        # time completion: 2.5-3 minutes completion time
+
+    # --------------------------------------------------------------------------
 
 
 
